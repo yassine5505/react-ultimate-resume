bb6d2628033d8d6d22faab02dc7f01f6adfb93be
Mohammed Yassine Chraibi <med.yassine@beebay.co>
2021-01-18 21:24:47 +0100

    Update cover image

--- src/package/components/cards/cards_types/projects/utils/images.js
+++ src/package/components/cards/cards_types/projects/utils/images.js
@@ -1,1 +1,2 @@
-export const DEFAULT_PROJECT_IMAGE = 'https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK';
+export const DEFAULT_PROJECT_IMAGE =
+    'https://images.unsplash.com/photo-1517503733723-8ea1cf616798?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80s';
